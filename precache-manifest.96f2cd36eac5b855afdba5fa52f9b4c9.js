8ba9369da1efe23ad04a23d4544d20516520d68e
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-05-22 00:49:07 +0530

    Updates

---
+++ precache-manifest.96f2cd36eac5b855afdba5fa52f9b4c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f6f5d82c5683635c18de35a4868281",
+    "url": "/pro-organiser-application/index.html"
+  },
+  {
+    "revision": "4c52d8dbdb3389148b82",
+    "url": "/pro-organiser-application/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5b6bcf818d8f69fdca1b",
+    "url": "/pro-organiser-application/static/css/main.6f181061.chunk.css"
+  },
+  {
+    "revision": "4c52d8dbdb3389148b82",
+    "url": "/pro-organiser-application/static/js/2.ba9b3d48.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pro-organiser-application/static/js/2.ba9b3d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6bcf818d8f69fdca1b",
+    "url": "/pro-organiser-application/static/js/main.abb6b7b5.chunk.js"
+  },
+  {
+    "revision": "6b1d8acea0a46705d23e",
+    "url": "/pro-organiser-application/static/js/runtime-main.ae569194.js"
+  }
+]);
